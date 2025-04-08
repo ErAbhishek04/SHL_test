110ee0fbb3c00ba501a879dee2456c81b0f0ef80
ErAbhishek04 <takawaneabhishek04@gmail.com>
2025-04-09 01:56:28 +0530

    final_push

--- backend/main.py
+++ backend/main.py
@@ -140,4 +140,5 @@ async def recommend_tests(q: Query):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
